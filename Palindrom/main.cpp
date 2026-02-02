7463ab80b8c27eb43551fa12bbb6b1b38018806e
SteelM25 <cmusa4031@gmail.com>
2026-02-02 23:53:54 +0300

    soruyu yazdım daha sonra bakacağım inşallah

---
+++ Palindrom/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+/*Sayı Ters Çevirme & Palindrom
+
+Soru:
+Kullanıcıdan bir tam sayı al.
+
+Sayının tersini hesapla
+
+Eğer sayı palindrom ise "Palindrome" yazdır
+
+📌 Örnek:
+
+Input: 12321
+Output: Palindrome
+
+
+📌 Amaç:
+
+Matematiksel işlemle digit ayırma
+
+while döngüsü
+
+Geçici değişken kullanımı*/
+
+int main(){
+
+    
+
+}
