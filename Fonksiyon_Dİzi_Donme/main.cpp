d6d7e79b6b78c938e42d55cb4aa8993c80949af8
SteelM25 <cmusa4031@gmail.com>
2026-02-04 20:47:03 +0300

    tamam

---
+++ Fonksiyon_Dİzi_Donme/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+/*Fonksiyon + Dizi (Birleştirme)
+
+Soru:
+Bir tamsayı dizisi ve boyut alan bir fonksiyon yaz:
+
+int countPositive(const int arr[], int size);
+
+
+Bu fonksiyon dizideki pozitif sayı adedini döndürsün.
+
+📌 Amaç:
+
+Dizi + fonksiyon birlikte kullanımı
+
+Parametre geçirme
+
+const farkındalığı*/
+
+int countPositive(const int arr[], int size){
+    int adet;
+    for(int i = 0; i < size; i++){   
+        if(arr[i] > 0) adet++;
+    }
+    return adet;
+}
+
+int main(){
+
+    int size;
+
+    cout << "Dizi boyutu: ";
+    cin >> size;
+
+    int dizi[size];
+
+    for (int a = 0; a < size; a++) {
+        cout << a + 1 << ". eleman: ";
+        cin >> dizi[a];    
+    }
+
+    cout << "\n\nPozitif sayı adeti: " << countPositive(dizi, size) << endl;
+
+
+}
