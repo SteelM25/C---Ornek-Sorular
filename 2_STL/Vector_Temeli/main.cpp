ffda734101903629785fb7733d68978eb507e15f
SteelM25 <cmusa4031@gmail.com>
2026-02-04 21:23:25 +0300

    soru yazıldı

---
+++ 2_STL/Vector_Temeli/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*Vector Temelleri
+Kullanıcıdan n sayı al, bir vector’e koy.
+
+Tek sayıları sil
+
+Kalanları ters sırada yazdır
+
+👉 erase-remove idiom’unu öğren.*/
+
+int main(){
+
+    
+    
+}
