84456f9e14262834175793ca7c17cf629fee4435
SteelM25 <cmusa4031@gmail.com>
2026-02-05 23:02:24 +0300

    soru yazıldı

---
+++ 2_STL/Deque_ile_kaydirmali_pencere/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+/*Deque ile Kaydırmalı Pencere
+Bir dizide k uzunluklu sliding window maksimumlarını yazdır.
+
+deque kullan
+
+Zaman karmaşıklığı: O(n)*/
+
+int main(){
+
+    
+
+}
